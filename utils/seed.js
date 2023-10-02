a25beb12612c591138e5f04c2adb600bc98cc265
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-10-02 22:39:01 +0530

    add hashedpassword in seed

--- utils/seed.js
+++ utils/seed.js
@@ -1,13 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
-
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 const prisma = new PrismaClient();
 
 async function main() {
+  const password = 'admin@ieee';
+  const salt = await bcrypt.genSalt(Number(process.env.HASH_ROUND));
+  const hashPassword = await bcrypt.hash(password, salt);
+
   await prisma.user.create({
     data: {
         name:"admin",
         email: 'admin@ieee.org',
-        password: 'admin@ieee',
+        password: hashPassword,
         phone:"123456789"
     },
   });
